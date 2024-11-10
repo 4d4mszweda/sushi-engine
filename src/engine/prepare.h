9e8cf919a013374b1b907deda967ab0b6a620dd4
Mikolaj <mikolajczyzak@gmail.com>
2024-11-10 09:56:54 +0100

    prepare: new pieces

--- src/engine/prepare.h
+++ src/engine/prepare.h
@@ -7,4 +7,17 @@
 
 typedef Move_sqares **Prepared_moves;
 
+typedef struct move_table
+{
+    Move_sqares w_pawn;
+    Move_sqares w_pawn_attack;
+    Move_sqares b_pawn;
+    Move_sqares b_pawn_attack;
+    Move_sqares knight;
+    Move_sqares bishop;
+    Move_sqares rook;
+    Move_sqares queen;
+    Move_sqares king;
+} move_table;
+
 #endif
