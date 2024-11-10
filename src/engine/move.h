ebab18573a28ab20f0f454c9e73db24be983777f
Mikolaj <mikolajczyzak@gmail.com>
2024-11-10 09:56:54 +0100

    prepare: new pieces

---
+++ src/engine/move.h
@@ -0,0 +1,7 @@
+#ifndef _MOVE_H_
+#define _MOVE_H_
+
+typedef unsigned long long int Move_sqares;
+typedef unsigned long long int Position;
+
+#endif
--- src/engine/prepare.c
+++ src/engine/prepare.c
@@ -15,33 +15,61 @@ void free_pm(Prepared_moves pm)
     free(pm);
 }
 
-Prepared_moves **prepare_knight()
+Move_sqares prepare_knight(int row, int col)
 {
-    Position starting_square = 0ULL;
     Move_sqares ms = 0ULL;
-    Prepared_moves pm = malloc_pm();
 
     int knight_moves[][2] = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}};
 
+    for (int km; km < 8; km++)
+    {
+        int move_row = row + knight_moves[row][col];
+        int move_col = col + knight_moves[row][col];
+
+        if (move_row >= 0 && move_row < 8 &&
+            move_col >= 0 && move_col < 8)
+        {
+            ms |= 1ULL << ((move_row << 3) + move_col);
+        }
+    }
+    ms = 0ULL;
+    return ms;
+}
+
+Prepared_moves prepare_w_pawn(int row, int col)
+{
+    Move_sqares ms = 1ULL;
+    return row == 1 ? ms << ((row << 3) + col) : ms << ((row << 4) + col);
+}
+
+Prepared_moves prepare_w_pawn_attack(int row, int col)
+{
+    Move_sqares ms = 0ULL;
+}
+
+Prepared_moves prepare_b_pawn(int row, int col)
+{
+    Move_sqares ms = 0ULL;
+}
+
+Prepared_moves prepare_b_pawn_attack(int row, int col)
+{
+    Move_sqares ms = 0ULL;
+}
+
+Prepared_moves prepare_king(int row, int col)
+{
+    Move_sqares ms = 0ULL;
+}
+
+move_table *generate_moves()
+{
+    move_table *mt;
     for (int row = 0; row < B_MAX_ROW; row++)
     {
         for (int column = 0; column < B_MAX_COL; column++)
         {
-            for (int km; km < 8; km++)
-            {
-                int move_row = row + knight_moves[row][column];
-                int move_col = column + knight_moves[row][column];
-
-                if (move_row >= 0 && move_row < 8 &&
-                    move_col >= 0 && move_col < 8)
-                {
-                    ms |= 1ULL << ((move_row << 3) + move_col);
-                }
-            }
-            pm[row][column] = ms;
-            starting_square++;
-            ms = 0ULL;
         }
     }
-    return pm;
+    return mt;
 }
